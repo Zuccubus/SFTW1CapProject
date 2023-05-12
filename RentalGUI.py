240db8f63c647083d23f57a753256724b92b9eab
Zuccubus <94533823+Zuccubus@users.noreply.github.com>
2023-05-13 07:09:17 +1000

    Add files via upload

---
+++ RentalGUI.py
@@ -0,0 +1,137 @@
+import tkinter
+from RentalModel import *
+
+class Rental_GUI:
+    def __init__(self):
+
+        self.window = tkinter.Tk()
+        self.window.title("New York Room Type Predictor")
+
+        self.first_frame = tkinter.Frame()
+        self.second_frame = tkinter.Frame()
+        self.third_frame = tkinter.Frame()
+        self.fourth_frame = tkinter.Frame()
+        self.fifth_frame = tkinter.Frame()
+        self.sixth_frame = tkinter.Frame()
+        self.seventh_frame = tkinter.Frame()
+        self.eighth_frame = tkinter.Frame()
+        self.ninth_frame = tkinter.Frame()
+        self.button_frame = tkinter.Frame()
+        self.output_frame = tkinter.Frame()
+
+
+        # neighbourhood
+        self.neighbourhood_label = tkinter.Label(self.first_frame, text="Property Neighbourhood: ")
+        self.neighbourhood_entry = tkinter.Entry(self.first_frame, bg="light blue", bd=2, width=10)
+        self.neighbourhood_label.pack(side="left")
+        self.neighbourhood_entry.pack(side="left")
+
+        # lat
+        self.latitude_label = tkinter.Label(self.second_frame, text="Enter Latitude: ")
+        self.latitude_entry = tkinter.Entry(self.second_frame, bg="light blue", bd=2, width=10)
+        self.latitude_label.pack(side="left")
+        self.latitude_entry.pack(side="left")
+
+        # long
+        self.longitude_label = tkinter.Label(self.third_frame, text="Enter Longitude: ")
+        self.longitude_entry = tkinter.Entry(self.third_frame, bg="light blue", bd=2, width=10)
+        self.longitude_label.pack(side="left")
+        self.longitude_entry.pack(side="left")
+
+        # days occupied in 2019
+        self.dOC_2019_label = tkinter.Label(self.fourth_frame, text="Enter Days Occupied in 2019: ")
+        self.dOC_2019_entry = tkinter.Entry(self.fourth_frame, bg="light blue", bd=2, width=10)
+        self.dOC_2019_label.pack(side="left")
+        self.dOC_2019_entry.pack(side="left")
+
+        # minimum nights to spend
+        self.min_nights_label = tkinter.Label(self.fifth_frame, text="Minimum Nights to Stay: ")
+        self.min_nights_entry = tkinter.Entry(self.fifth_frame, bg="light blue", bd=2, width=10)
+        self.min_nights_label.pack(side="left")
+        self.min_nights_entry.pack(side="left")
+
+        # num of reviews
+        self.num_rev_label = tkinter.Label(self.sixth_frame, text="Number of Reviews: ")
+        self.num_rev_entry = tkinter.Entry(self.sixth_frame, bg="light blue", bd=2, width=10)
+        self.num_rev_label.pack(side="left")
+        self.num_rev_entry.pack(side="left")
+
+        # monthly reviews
+        self.monthly_rev_label = tkinter.Label(self.seventh_frame, text="number of Monthly Reviews: ")
+        self.monthly_rev_entry = tkinter.Entry(self.seventh_frame, bg="light blue", bd=2, width=10)
+        self.monthly_rev_label.pack(side="left")
+        self.monthly_rev_entry.pack(side="left")
+
+        # property availablity in 2020
+        self.availability_label = tkinter.Label(self.eighth_frame, text="2020 Property Availability: ")
+        self.availability_entry = tkinter.Entry(self.eighth_frame, bg="light blue", bd=2, width=10)
+        self.availability_label.pack(side="left")
+        self.availability_entry.pack(side="left")
+
+        # price
+        self.price_label = tkinter.Label(self.ninth_frame, text="Rental Price: ")
+        self.price_entry = tkinter.Entry(self.ninth_frame, bg="light blue", bd=2, width=10)
+        self.price_label.pack(side="left")
+        self.price_entry.pack(side="left")
+
+        # buttons
+        self.predict_button = tkinter.Button(self.button_frame, text="Predict",
+                                             command=self.predict_room)
+        self.quit_button = tkinter.Button(self.button_frame, text="Quit",
+                                          command=self.window.destroy)
+
+        self.predict_button.pack(side="left")
+        self.quit_button.pack(side="left")
+
+        # output
+        self.results = tkinter.Text(self.output_frame, bg="light blue", height=10, width=40)
+        self.results.pack()
+
+        # packing frames
+        self.first_frame.pack()
+        self.second_frame.pack()
+        self.third_frame.pack()
+        self.fourth_frame.pack()
+        self.fifth_frame.pack()
+        self.sixth_frame.pack()
+        self.seventh_frame.pack()
+        self.eighth_frame.pack()
+        self.ninth_frame.pack()
+        self.button_frame.pack()
+        self.output_frame.pack()
+
+        tkinter.mainloop()
+
+    def predict_room(self):
+
+        self.results.delete(0.0, tkinter.END)
+
+        neighbourhood = self.neighbourhood_entry.get()
+        lat = self.latitude_entry.get()
+        long = self.longitude_entry.get()
+        dOC_2019 = self.dOC_2019_entry.get()
+        min_nights = self.min_nights_entry.get()
+        num_rev = self.num_rev_entry.get()
+        monthly_rev = self.monthly_rev_entry.get()
+        availability = self.availability_entry.get()
+        price = self.price_entry.get()
+
+
+        property_info = (neighbourhood, lat, long, dOC_2019, min_nights, num_rev, monthly_rev,
+                         availability, price)
+        room_type_prediction = best_model.predict([property_info])
+
+        # 4 types 0 = house, 1 = hotel, 2= private, 3 = shared
+
+        if room_type_prediction == 0.0:
+            room_type = "Entire house/apt"
+        elif room_type_prediction == 1.0:
+            room_type = "Hotel room"
+        elif room_type_prediction == 2.0:
+            room_type = "Private room"
+        else:
+            room_type = "Shared room"
+
+        self.results.insert(f"Room type is predicted to be {room_type}")
+
+my_rental_GUI = Rental_GUI()
---
+++ RentalModel.py
@@ -0,0 +1,115 @@
+import sklearn
+from sklearn.utils import shuffle
+from sklearn import datasets
+from sklearn import svm
+from sklearn import linear_model, preprocessing
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pyplot
+from matplotlib import style
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import accuracy_score
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+
+my_property_data = pd.read_csv("NY Realstate Pricing.csv")
+
+predict = "room_type"
+
+
+le = preprocessing.LabelEncoder()
+F1 = le.fit_transform(list(my_property_data["F1"])) # identifier
+id = le.fit_transform(list(my_property_data["id"])) # identifier
+neighbourhood = le.fit_transform(list(my_property_data["neighbourhood"])) # location
+latitude = le.fit_transform(list(my_property_data["latitude"])) # location
+longitude = le.fit_transform(list(my_property_data["longitude"])) # location
+days_occupied_in_2019 = le.fit_transform(list(my_property_data["days_occupied_in_2019"]))
+minimum_nights = le.fit_transform(list(my_property_data["minimum_nights"]))
+number_of_reviews = le.fit_transform(list(my_property_data["number_of_reviews"]))
+reviews_per_month = le.fit_transform(list(my_property_data["reviews_per_month"]))
+availability_2020 = le.fit_transform(list(my_property_data["availability_2020"]))
+price = le.fit_transform(list(my_property_data["price"])) # price in USD
+room_type = le.fit_transform(list(my_property_data["room_type"]))
+# 4 types 0 = house, 1 = hotel, 2= private, 3 = shared
+
+x = list(zip(neighbourhood, latitude, longitude, days_occupied_in_2019, minimum_nights, number_of_reviews,
+             reviews_per_month, availability_2020, price))
+y = list(room_type)
+
+num_folds = 5
+seed = 7
+
+scoring = "accuracy"
+
+import sklearn.model_selection
+
+# seperating dataset into test set (80%) and train set (20%)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.20, random_state=seed)
+
+# list of models used
+predictive_models = []
+
+# four models that will be tested for predictive accuracy
+predictive_models.append(("NB", GaussianNB()))
+predictive_models.append(("SVM", SVC()))
+predictive_models.append(("RF", RandomForestClassifier()))
+predictive_models.append(("GBM", GradientBoostingClassifier()))
+
+model_results = []
+model_names = []
+
+
+print("Performance on Training set")
+# go through model list and use each one
+for name, model in predictive_models:
+    kfold = KFold(n_splits=num_folds,shuffle=True,random_state=seed)
+    cv_results = cross_val_score(model, x_train, y_train, cv=kfold, scoring="accuracy")
+    # adding model name and accuracy to lists
+    model_results.append(cv_results)
+    model_names.append(name)
+    # outputing results of model
+    print(f"{name}: {cv_results.mean():,.6f} ({cv_results.std():,.6f})\n")
+
+
+fig = pyplot.figure()
+fig.suptitle("Predictive Algorithm Comparison")
+ax = fig.add_subplot(111)
+pyplot.boxplot(model_results)
+ax.set_xticklabels(model_names)
+pyplot.show()
+
+predictive_models.append(("RF", RandomForestClassifier))
+randf = RandomForestClassifier()
+
+best_model = randf
+best_model.fit(x_train, y_train)
+y_pred = best_model.predict(x_test)
+print(f"Best Model Accuracy Score on Test Set: {accuracy_score(y_test, y_pred)}")
+
+print(classification_report(y_test, y_pred))
+
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+cm = confusion_matrix(y_test, y_pred)
+cm_display = ConfusionMatrixDisplay(confusion_matrix=cm)
+cm_display.plot()
+pyplot.show()
+
+for x in range(len(y_pred)):
+    print(f"Predicted: {y_pred[x]} Actual: {y_test[x]} Data: {x_test[x]}")
+
